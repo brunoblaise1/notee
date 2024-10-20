523378bc65a8d7a66135ca3de6e05f1a4ae3decb
brunoblaise <brunoblaise94@gmail.com>
2024-10-20 16:35:32 +0200

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 "use client"
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import ScanbotSDKService from "./services/scanbot-sdk-service";
+
 
 export default function Home() {
   const [data, setData] = useState("Not Found");
+
+  useEffect(() => {
+		async function loadSDK() {
+			await ScanbotSDKService.instance.initialize();
+			
+		}
+		loadSDK();
+	}, [])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <>
+      {/* <>
       <BarcodeScannerComponent
         width={500}
         height={500}
@@ -17,7 +27,7 @@ export default function Home() {
         }}
       />
       <p>{data}</p>
-    </>
+    </> */}
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
